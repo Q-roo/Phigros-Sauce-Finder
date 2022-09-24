450db6cc54ec83d15e46100c51365460095f015d
Q-roo <109001787+Q-roo@users.noreply.github.com>
2022-09-24 19:00:22 +0200

    Create MainMenu.cs

---
+++ PhigrosSauceFinder/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhigrosSauceFinder
+{
+    class MainMenu
+    {
+        void WriteOption(string optionID, string optionName)
+        {
+            Console.WriteLine($"[{optionID}] {optionName}");
+        }
+
+        void Menu()
+        {
+            WriteOption("1", "Calculate sauce score");
+            WriteOption("2", "Calculate minimum requirement");
+
+            string input = Console.ReadLine();
+
+            if (input == "1")
+            {
+
+            }
+
+            else if (input == "2")
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("Choose a valid option!");
+            }
+        }
+    }
+}
